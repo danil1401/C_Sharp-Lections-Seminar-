941955774c0acf2db66f1b9c50eaf205cf15f49c
danil1401 <danil1401@yandex.ru>
2022-09-30 00:26:52 +0400

    Example005 Сделали Особое приветствие

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите им пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") // ToLower- позволяет все символы строки перевести в нижний регистр
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
