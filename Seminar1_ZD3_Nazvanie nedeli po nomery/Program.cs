4f7b8fdafa898b86a5cb6b6093e9e9b1e1a8e011
danil1401 <danil1401@yandex.ru>
2022-10-02 17:13:45 +0400

    Задача Название дня недели

---
+++ Seminar1_ZD3_Nazvanie nedeli po nomery/Program.cs
@@ -0,0 +1,45 @@
+﻿//2. Написать программу, которая будет выдавать название дня недели по заданному номеру 
+//3 -> Среда
+//5 -> Пятница
+
+Console.Write("Введите номер дня недели (от 1 до 7): ");
+int weekDay=Convert.ToInt32(Console.ReadLine());
+while (weekDay<1|weekDay>7){
+    Console.Write("Вы ввели недопустимый номер дня недели, повторите ввод (от 1 до 7): ");
+    weekDay=Convert.ToInt32(Console.ReadLine());
+}
+if (weekDay==1) Console.WriteLine("Понедельник");
+else if (weekDay==2) Console.WriteLine("Вторник");
+else if (weekDay==3) Console.WriteLine("Среда");
+else if (weekDay==4) Console.WriteLine("Четверг");
+else if (weekDay==5) Console.WriteLine("Пятница");
+else if (weekDay==6) Console.WriteLine("Суббота");
+else if (weekDay==7) Console.WriteLine("Воскресенье");
+else Console.WriteLine("Вы ввели недопустимый номер дня недели");
+
+/* Альтернативный вариант
+Console.Write("Введите номер дня недели (от 1 до 7): ");
+int weekDay=Convert.ToInt32(Console.ReadLine());
+while (a<1|a>7){
+    Console.Write("Вы ввели недопустимый номер дня недели, повторите ввод (от 1 до 7): ");
+    a=Convert.ToInt32(Console.ReadLine());
+}
+switch (a){
+    case 1:Console.WriteLine("Понедельник");
+    break;
+    case 2:Console.WriteLine("Вторник");
+    break;
+    case 3:Console.WriteLine("Среда");
+    break;
+    case 4:Console.WriteLine("Четверг");
+    break;
+    case 5:Console.WriteLine("Пятница");
+    break;
+    case 6:Console.WriteLine("Суббота");
+    break;
+    case 7:Console.WriteLine("Воскресенье");
+    break;
+        aefualt:Console.WriteLine("Вы ввели недопустимый номер дня недели");
+        break;
+
+} */
