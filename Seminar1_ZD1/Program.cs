e83f6844bef1bf6066d77ddf51dc7f0325f49687
danil1401 <danil1401@yandex.ru>
2022-10-02 16:51:54 +0400

    ZD1

--- Seminar1_ZD1/Program.cs
+++ Seminar1_ZD1/Program.cs
@@ -1,6 +1,7 @@
-﻿// Console.WriteLine("Введите целое число");
+﻿// Программа нахождение квадрата одного числа
+// Console.WriteLine("Введите целое число");
 //int n = Convert.ToInt32(Console.Readline());
 //Console.Write(n*n);
-Console.WriteLine("5");
+Console.WriteLine("Введите целое число");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.Write(n*n);
