8559c87cda425be90176e60e6c375e0410597e42
danil1401 <danil1401@yandex.ru>
2022-10-02 16:26:03 +0400

    Задача на нахождение квадрата числа

---
+++ Seminar1_ZD1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Console.WriteLine("Введите целое число");
+//int n = Convert.ToInt32(Console.Readline());
+//Console.Write(n*n);
+Console.WriteLine("5");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(n*n);
