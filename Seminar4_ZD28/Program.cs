577f755d09842a16e4cb177b7ca4a6dbd54a459a
danil1401 <danil1401@yandex.ru>
2022-10-12 22:32:24 +0400

    Решение Задача 28. Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N

---
+++ Seminar4_ZD28/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задача 28. Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N
+
+Console.Clear();
+
+//РЕШЕНИЕ ПРЕПОДАВАТЕЛЯ
+
+Console.WriteLine("Введите число: ");
+int N=int.Parse(Console.ReadLine());
+int res=1;
+int start=1;
+while (start<=N)
+{
+    res=res*start;
+    start++;
+}
+Console.WriteLine($"Произведение чисел числа {N} = {res}");
+
+
+//РЕШЕНИЕ(Не работает почему то ) в ГРУППЕ
+/*Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+int result = addition(num);
+
+int addition(int number)
+{
+    int count = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        count *= i; // либо записать count =count * i-одно и тоже
+    }
+    return count;
+}
+
+Console.WriteLine(result);*/
