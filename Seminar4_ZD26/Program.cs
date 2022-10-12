90acbc0adff62231e130ddc70aa1d8c4c2b42faf
danil1401 <danil1401@yandex.ru>
2022-10-12 21:57:46 +0400

    Задача 26 Напишите программу, которая принимает на вход число и выдает колиество цифр в числе

---
+++ Seminar4_ZD26/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 26 Напишите программу, которая принимает на вход число и выдает колиество цифр в числе.
+
+//РЕШЕНИЕ ПРЕПОДОВАТЕЛЯ
+Console.Clear();
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (a != 0)
+{
+    a = a / 10;
+    count++;
+}
+
+Console.WriteLine(count);
+
+
+
+/*АЛЬТЕРНАТИВНОЕ РЕШЕНИЕ
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());// считываем с консоли и вводим значение в num
+
+int count = 0;// здесь заводим переменную count- наш счетчик, нужна для того что бы считала сколько раз мы делили наше число
+
+if (num==0)
+{
+    count=1;
+}
+else while (num!=0)// != означает не равно 0
+{
+    num=num/10;
+    count++;
+}*/
+
+
+//Console.WriteLine(count);
