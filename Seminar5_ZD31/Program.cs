f84dca6cf806d87856d958d9c4c4c6ccb52d16b7
danil1401 <danil1401@yandex.ru>
2022-10-16 15:36:02 +0400

    Задача 31. Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9,9]. Найдите сумму отрицательных и положительных элементов массива.

---
+++ Seminar5_ZD31/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 31. Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9,9]. Найдите сумму отрицательных и положительных элементов массива.
+//Например в массиве[3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+
+Console.Clear();
+
+int[]array=new int[12];
+int len =array.Length;
+int index =0;
+int respos=0;
+int resneg=0;
+
+while(index<len)
+{
+    array[index]=new Random().Next(-10,10);
+    index++;
+}
+
+for(int i=0;i<len;i++)
+{
+    if(array[i]<0)
+    {
+        resneg+=array[i];
+    }
+    else
+    {
+        respos +=array[i];
+
+
+    }
+    Console.Write(array[i]+ " ");
+}
+Console.Write(" \n");
+Console.WriteLine($"Сумма положительных = {respos}, сумма отрицательных= = {resneg}");
