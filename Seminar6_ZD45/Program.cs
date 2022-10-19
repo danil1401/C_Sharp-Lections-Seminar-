12f45c384e45687cf971d0768b1047cce4853ad4
danil1401 <danil1401@yandex.ru>
2022-10-20 00:08:00 +0400

    РЕШЕНИЕ //Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования

--- Seminar6_ZD45/Program.cs
+++ Seminar6_ZD45/Program.cs
@@ -2,3 +2,35 @@
 
 Console.Clear();
 
+Console.WriteLine("Введите длину массива: ");
+int length=int.Parse(Console.ReadLine());
+int [] array=new int[length];
+
+for (int i=0;i< array.Length;i++)
+{
+    array[i]= new Random().Next(100,1000);
+}
+
+void PrintArray(int[]array)
+{
+    for(int i=0;i<array.Length;i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+}
+
+int [] ArrayCopy(int[] array)
+{
+    int[]ResArr=new int [length];
+    for (int i=0;i<length;i++)
+    {
+        ResArr[i]=array[i];
+    }
+    return ResArr;
+}
+
+Console.Write("Ваш массив: ");
+PrintArray(array);
+Console.WriteLine();
+Console.Write("Копия вашего массива: ");
+PrintArray(ArrayCopy(array));
