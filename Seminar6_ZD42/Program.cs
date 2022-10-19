e6c190e99572c9a2642e20ae801fc15b9eab1e7e
danil1401 <danil1401@yandex.ru>
2022-10-19 23:20:54 +0400

    Решение // Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное

---
+++ Seminar6_ZD42/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное// 45->101101
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string count = null;
+while (number != 0)
+{
+    count += number % 2; // делим с остатком-если остатка от деления нет выведет 0
+    number /= 2;
+}
+for (int i = count.Length - 1; i >= 0; i--) //В обратном порядке для разворота полученного двоичного числа
+{
+    Console.Write(count[1]);
+}
