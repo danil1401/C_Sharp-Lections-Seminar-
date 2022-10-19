5d1ff682bbf56de1e51c6a96c1bb06e988bbdadc
danil1401 <danil1401@yandex.ru>
2022-10-19 23:58:23 +0400

    Решение //Задача 44: Не используя рекурсию, ввыведите первые N чисел Фибоначи. первые два числа Фибоначи 0 и 1 ДОПОЛНЕНИЕ

--- Seminar6_ZD44/Program.cs
+++ Seminar6_ZD44/Program.cs
@@ -17,3 +17,20 @@ for (int i = 1; i < fib - 1; i++)
     first = second;
     second = next;
 }
+
+//////////РЕШЕНИЕ ПРЕПОДАВАТЕЛЯ
+/*int number=20;
+int firstElem=1;
+int secondElem=1;
+
+Console.WriteLine("Последовательность фибоначчи");
+Console.WriteLine($"1. {firstElem}");
+Console.WriteLine($"2. {secondElem}");
+
+for (int i=3;i<=number; i++)
+{
+    int nextElem=firstElem+secondElem;
+    Console.WriteLine($"{i}. {nextElem}");
+    firstElem= secondElem;
+    secondElem=nextElem;
+}*/
