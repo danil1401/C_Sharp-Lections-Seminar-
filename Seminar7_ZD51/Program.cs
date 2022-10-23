7d4da192d63b314c27ae4306809fdb798dff7e06
danil1401 <danil1401@yandex.ru>
2022-10-23 20:42:43 +0400

    / Задача 51. Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали.

--- Seminar7_ZD51/Program.cs
+++ Seminar7_ZD51/Program.cs
@@ -1,5 +1,4 @@
-﻿//Задача 49. Задайте двумерный массив. Найдите элементы, у которых оба индекса четные, и замените эти элементы на их квадраты
-
+﻿// Задача 51. Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали.
 
 Console.Clear();
 
@@ -21,19 +20,6 @@ void Fill(int[,] matr)
     }
 }
 
-Fill(matr);
-Console.WriteLine();
-Console.WriteLine("Исходная матрица: ");
-Print(matr);
-
-for (int i = 0; i < matr.GetLength(0); i += 2)
-{
-    for (int j = 0; j < matr.GetLength(1); j += 2)
-    {
-        matr[i, j] = i + j;
-    }
-}
-
 void Print(int[,] matr)
 {
     for (int i = 0; i < matr.GetLength(0); i++)
@@ -46,5 +32,21 @@ void Print(int[,] matr)
     }
 }
 
-Console.WriteLine("Измененная матрица: ");
+Console.WriteLine("Исходная матрица: ");
+Fill(matr);
 Print(matr);
+
+int sum = 0;
+Console.WriteLine();
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        if (i == j)
+        {
+            sum = sum + matr[i, j];
+        }
+    }
+}
+
+Console.WriteLine("Сумма элементов главной диагонали= " + sum);
