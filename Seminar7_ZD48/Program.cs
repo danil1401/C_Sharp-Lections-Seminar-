908fe0ebc452244b09a842a2094d25982b035887
danil1401 <danil1401@yandex.ru>
2022-10-23 20:14:41 +0400

    //Задача 48. Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Amn=m+n. Введите полученный массив на экран

---
+++ Seminar7_ZD48/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 48. Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Amn=m+n. Введите полученный массив на экран.
+
+Console.Clear();
+
+Console.Write("Введите высоту матрицы: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите длину матрицы ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+void FillArray(int[,] mart)
+{
+    for (int i = 0; i < mart.GetLength(0); i++)
+    {
+        for (int j = 0; j < mart.GetLength(1); j++)
+        {
+            mart[i,j]=i+j;
+        }
+    }
+}
+
+void Print(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray(array);
+Print(array);
+
